016c8af1f1c1354dfb2577e3e25d43e80555e6d6
Matt Hickford <matt.hickford@gmail.com>
2012-10-08 23:56:25 +0100

    still wasn't forking right :(

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,4 +1,22 @@
 worker_processes 3
-preload_app true
+preload_app false
 timeout 30
 
+before_fork do |server, worker|
+  # the following is highly recomended for Rails + "preload_app true"
+  # as there's no need for the master process to hold a connection
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
+end
+
+after_fork do |server, worker|
+  # the following is *required* for Rails + "preload_app true",
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
+
+  # if preload_app is true, then you may also want to check and
+  # restart any other shared sockets/descriptors such as Memcached,
+  # and Redis.  TokyoCabinet file handles are safe to reuse
+  # between any number of forked children (assuming your kernel
+  # correctly implements pread()/pwrite() system calls)
+end
